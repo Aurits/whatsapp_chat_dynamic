fb17e51c543f37f3a31950f272007ed7989fa4b4
Aurits <87270662+Aurits@users.noreply.github.com>
2023-08-29 15:02:38 +0300

    commit

--- index.php
+++ index.php
@@ -6,47 +6,70 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="styles.css">
   <title>Chatbot Application</title>
+  <style>
+  </style>
 </head>
 
 <body>
   <div class="container">
     <div class="chat-container">
-       <header class="app-header">
+      <header class="app-header">
         <h1 class="app-title">WeFed</h1>
         <div class="app-actions">
           <button class="app-action-button">Settings</button>
           <button class="app-action-button">Profile</button>
         </div>
-       </header>
+      </header>
       <div class="chat">
         <div class="">
-          <p style="text-align: center;">Welcome to the WeFed! Let's get started.</p>
+          <p style="text-align: center;">Welcome to WeFed! Let's get started.</p>
         </div>
       </div>
       <div id="radioOptions" class="radio-options">
-          <!-- Radio options will be dynamically added here -->
-        </div>
+        <!-- Radio options will be dynamically added here -->
+      </div>
     </div>
 
     <div class="input-container">
       <div id="inputGroup" class="input-group">
-        <input type="text" style="min-width: 40vw" class="input-box" placeholder="Type your response..." id="userInput">
+        <input type="text" style="min-width: 40vw" class="input-box" placeholder="Type your response..." id="userInput" onkeydown="if (event.key === 'Enter') { event.preventDefault(); sendUserResponse(); }" />
       </div>
-      <button class="send-button" style="margin-right: 30px" onclick="sendUserResponse()">Send</button>
+      <button class="send-button" style="margin-right: 30px" id="sendButton" onclick="sendUserResponse()">
+        Send
+      </button>
+    </div>
+
+    <div id="submitButtonContainer" class="input-container" style="display: none;">
+      <button class="send-button" style="margin-right: 30px" id="submitButton" onclick="submitForm()">
+        Submit
+      </button>
     </div>
   </div>
 
   <script>
     const chatContainer = document.querySelector('.chat');
     const inputGroup = document.getElementById('inputGroup');
+    const userInput = document.getElementById('userInput');
     const radioOptions = document.getElementById('radioOptions');
-    const questions = [
-      { question: "What's your favorite color?", options: [] },
-      { question: "Do you like dogs or cats?", options: ['Dogs', 'Cats'] },
-      { question: "What's your hobby?", options: [] },
+    const sendButton = document.getElementById('sendButton');
+    const submitButton = document.getElementById('submitButton');
+    const submitButtonContainer = document.getElementById('submitButtonContainer');
+    const questions = [{
+        question: "What's your favorite color?",
+        options: []
+      },
+      {
+        question: "Do you like dogs or cats?",
+        options: ['Dogs', 'Cats']
+      },
+      {
+        question: "What's your hobby?",
+        options: []
+      },
       // Add more questions here
     ];
     let currentQuestionIndex = 0;
+    let allQuestionsAsked = false;
 
     function showCurrentQuestion() {
       inputGroup.style.display = 'block';
@@ -55,7 +78,7 @@
       questionElement.className = "message user-a";
       questionElement.innerHTML = `
         <p>${currentQuestion.question}</p>
-        <span class="timestamp"><?php echo date('h:i A'); ?></span>
+        <span class="timestamp">${new Date().toLocaleTimeString()}</span>
       `;
       chatContainer.appendChild(questionElement);
 
@@ -70,6 +93,16 @@
           radioOptions.appendChild(radioOption);
         }
       }
+
+      if (currentQuestionIndex == questions.length - 1) {
+        allQuestionsAsked = true;
+        // sendButton.style.display = 'none';
+
+        // submitButtonContainer.style.display = 'block';
+      } else {
+        sendButton.style.display = 'block';
+        submitButtonContainer.style.display = 'none';
+      }
     }
 
     function sendUserResponse() {
@@ -82,7 +115,7 @@
         responseElement.className = "message user-b";
         responseElement.innerHTML = `
           <p>${response}</p>
-          <span class="timestamp"><?php echo date('h:i A'); ?></span>
+          <span class="timestamp">${new Date().toLocaleTimeString()}</span>
         `;
         chatContainer.appendChild(responseElement);
 
@@ -93,8 +126,14 @@
         } else {
           // All questions have been asked
           inputGroup.style.display = 'none';
+          sendButton.style.display = 'none';
+          submitButtonContainer.style.display = 'block';
         }
       }
+    }
+
+    function submitForm() {
+      console.log("Form submitted");
     }
 
     // Initial question display
@@ -103,5 +142,3 @@
 </body>
 
 </html>
-
-
