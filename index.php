834a03181a178bd5ad076f9aa9c85334d94fda91
Aurits <87270662+Aurits@users.noreply.github.com>
2023-08-29 16:10:57 +0300

    commit

--- index.php
+++ index.php
@@ -8,42 +8,130 @@
   <title>Chatbot Application</title>
   <style>
   </style>
+
+  <script>
+   // Show the Settings page and hide other pages
+    function showSettings() {
+      document.querySelector('.chat-container').style.display = 'none';
+      document.querySelector('.settings-container').style.display = 'block';
+      document.querySelector('.profile-container').style.display = 'none';
+      document.querySelector('.input-container').style.display = 'none';
+    }
+
+    // Show the Profile page and hide other pages
+    function showProfile() {
+      document.querySelector('.chat-container').style.display = 'none';
+      document.querySelector('.settings-container').style.display = 'none';
+      document.querySelector('.profile-container').style.display = 'block';
+      document.querySelector('.input-container').style.display = 'none';
+    }
+  </script>
 </head>
 
 <body>
   <div class="container">
-    <div class="chat-container">
-      <header class="app-header">
-        <h1 class="app-title">WeFed</h1>
-        <div class="app-actions">
-          <button class="app-action-button">Settings</button>
-          <button class="app-action-button">Profile</button>
+        
+        <div class="chat-container">
+          <header class="app-header">
+            <h1 class="app-title">WeFed</h1>
+            <div class="app-actions">
+           <button class="app-action-button" onclick="showSettings()">Settings</button>
+              <button class="app-action-button" onclick="showProfile()">Profile</button>
+            </div>
+          </header>
+          <div class="chat">
+            <div class="">
+              <p style="text-align: center;">Welcome to WeFed! Let's get started.</p>
+            </div>
+          </div>
+          <div id="radioOptions" class="radio-options">
+            <!-- Radio options will be dynamically added here -->
+          </div>
         </div>
-      </header>
-      <div class="chat">
-        <div class="">
-          <p style="text-align: center;">Welcome to WeFed! Let's get started.</p>
+
+        <div class="input-container">
+          <div id="inputGroup" class="input-group">
+            <input type="text" style="min-width: 40vw" class="input-box" placeholder="Type your response..." id="userInput" onkeydown="if (event.key === 'Enter') { event.preventDefault(); sendUserResponse(); }" />
+          </div>
+          <button class="send-button" style="margin-right: 30px" id="sendButton" onclick="sendUserResponse()">
+            Send
+          </button>
         </div>
-      </div>
-      <div id="radioOptions" class="radio-options">
-        <!-- Radio options will be dynamically added here -->
-      </div>
-    </div>
 
-    <div class="input-container">
-      <div id="inputGroup" class="input-group">
-        <input type="text" style="min-width: 40vw" class="input-box" placeholder="Type your response..." id="userInput" onkeydown="if (event.key === 'Enter') { event.preventDefault(); sendUserResponse(); }" />
-      </div>
-      <button class="send-button" style="margin-right: 30px" id="sendButton" onclick="sendUserResponse()">
-        Send
-      </button>
-    </div>
+        <div id="submitButtonContainer" class="input-container" style="display: none;">
+          <button class="send-button" style="margin-right: 30px" id="submitButton" onclick="submitForm()">
+            Submit
+          </button>
+        </div>
 
-    <div id="submitButtonContainer" class="input-container" style="display: none;">
-      <button class="send-button" style="margin-right: 30px" id="submitButton" onclick="submitForm()">
-        Submit
-      </button>
-    </div>
+        <!-- Settings page -->
+        <div class="settings-container" style="display: none;">
+          <header class="app-header">
+            <h1 class="app-title">WeFed</h1>
+            <div class="app-actions">
+           <button class="app-action-button" onclick="showSettings()">Settings</button>
+              <button class="app-action-button" onclick="showProfile()">Profile</button>
+            </div>
+          </header>
+          <!-- Settings content -->
+          <h2 class="page-title">Settings</h2>
+          <p class="page-description">Welcome to the Settings page, where you can customize your app experience.</p>
+          <div class="section">
+            <h3 class="section-title">General Settings</h3>
+            <p class="section-description">Adjust the general settings that suit your preferences:</p>
+            <ul class="settings-list">
+              <li class="setting">Theme: Choose between light and dark themes.</li>
+              <li class="setting">Language: Set your preferred language for the app.</li>
+            </ul>
+          </div>
+          <div class="section">
+            <h3 class="section-title">Notification Settings</h3>
+            <p class="section-description">Manage your notification preferences:</p>
+            <ul class="settings-list">
+              <li class="setting">Push Notifications: Enable or disable push notifications.</li>
+              <li class="setting">Email Notifications: Opt in or out of email notifications.</li>
+            </ul>
+          </div>
+          <!-- Add more content as needed -->
+        </div>
+
+        <!-- Profile page -->
+        <div class="profile-container" style="display: none;">
+          <header class="app-header">
+            <h1 class="app-title">WeFed</h1>
+            <div class="app-actions">
+           <button class="app-action-button" onclick="showSettings()">Settings</button>
+              <button class="app-action-button" onclick="showProfile()">Profile</button>
+            </div>
+          </header>
+          <!-- Profile content -->
+          <h2 class="page-title">Your Profile</h2>
+          <p class="page-description">Welcome to your profile page, where you can view and manage your information.</p>
+          <div class="profile-details">
+            <img src="background.png" width="70px" height="70px" style="border-radius: 48%" alt="Your Profile Picture" class="profile-picture">
+            <div class="profile-info">
+              <h3 class="profile-name">John Doe</h3>
+              <p class="profile-field">Email: john.doe@example.com</p>
+              <p class="profile-field">Location: New York City, USA</p>
+              <p class="profile-bio">Bio: Exploring the world one code at a time.</p>
+            </div>
+          </div>
+          <h3 class="section-title">Edit Profile</h3>
+          <p class="section-description">Update your profile details:</p>
+          <form class="edit-profile-form">
+            <label for="name" class="form-label">Name:</label>
+            <input type="text" id="name" name="name" class="form-input" value="John Doe">
+            <label for="email" class="form-label">Email:</label>
+            <input type="email" id="email" name="email" class="form-input" value="john.doe@example.com">
+            <label for="location" class="form-label">Location:</label>
+            <input type="text" id="location" name="location" class="form-input" value="New York City, USA">
+            <label for="bio" class="form-label">Bio:</label>
+            <textarea id="bio" name="bio" class="form-textarea">Exploring the world one code at a time.</textarea>
+            <button class="update-button">Update Profile</button>
+          </form>
+          <!-- Add more content as needed -->
+        </div>
+
   </div>
 
   <script>
